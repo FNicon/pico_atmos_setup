24cf83ac8798396b1c69652e4665217d1b44e316
fnicon <mybeddrom@gmail.com>
2026-03-29 17:00:32 +0900

    update atmos env pico

--- resources/nix/atmos/atmos/env/pico/exs/across.lua
+++ resources/nix/atmos/atmos/env/pico/exs/across.lua
@@ -1,16 +1,15 @@
 require "atmos"
-local env  = require "atmos.env.pico"
+require "atmos.env.pico"
 local pico = require "pico"
 
-pico.set.size.window({x=500,y=500}, {x=100,y=100})
-
-local pt1 = pico.pos(0,0)
-local pt2 = pico.pos(100,100)
-local pt = {x=0, y=0}
-
-local dy = (pt2.y - pt1.y) / (pt2.x - pt1.x)
+pico.set.window { dim = {'!', w=500, h=500} }
 
 loop(function()
+    local pt1 = {x=0, y=0}
+    local pt2 = {x=100, y=100}
+    local pt = {'!', x=0, y=0}
+
+    local dy = (pt2.y - pt1.y) / (pt2.x - pt1.x)
     par_or(function()
         every('draw',function()
             pico.output.draw.pixel(pt)
--- resources/nix/atmos/atmos/env/pico/init.lua
+++ resources/nix/atmos/atmos/env/pico/init.lua
@@ -4,18 +4,17 @@ local pico  = require "pico"
 pico.zet = pico.set     -- because of `set` keyword in Atmos
 
 local M = {
-    mpf = 25,   -- 0: as fast as possible
+    fps = 30,
     now = 0,
-    ren = nil,
     mode = { primary=true, secondary=true },
 }
 
-function M.open ()
-    pico.init(true)
-    pico.set.expert(true)
-end
+pico.init(true)
+pico.set.expert(true, M.fps)
 
-local MS = M.mpf
+function M.quit ()
+    pico.init(false)
+end
 
 local meta = {
     __atmos = function (awt, e)
@@ -42,30 +41,16 @@ local meta = {
     end
 }
 
-function M.close ()
-    pico.init(false)
-end
-
 function M.step ()
     local mcur = M.mode and M.mode.current
 
-    local old = pico.get.ticks()
-    local e = pico.input.event((mcur == 'secondary') and 0 or MS)
-    local cur = pico.get.ticks()
-
-    if mcur ~= 'secondary' then
-        if M.mpf == 0 then
-            local dt = (cur - M.now)
-            M.now = cur
-            emit('clock', dt, M.now)
-        else
-            MS = MS - (cur-old)
-            if MS <= 0 then
-                MS = M.mpf + MS
-                M.now = cur
-                emit('clock', M.mpf, M.now)
-            end
-        end
+    local e, ms
+    if mcur == 'secondary' then
+        e = pico.input.event(0)
+    else
+        e,ms = pico.input.event()
+        M.now = pico.get.now()
+        emit('clock', ms, M.now)
     end
 
     if e then
